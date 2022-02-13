323d34463d242b9b4b86baca3a380016e94d32c0
BuildTools <unconfigured@null.spigotmc.org>
2022-02-13 02:32:09 +0100

    loader final

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var loader = document.querySelector(".loader")
+var test = document.querySelector(".test")
 
 window.addEventListener("load", vanish);
 
@@ -6,9 +7,9 @@ function Sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-
 async function vanish() {
     await Sleep(2000);
-    loader.classList.add("disppear");
+    loader.classList.add("disappear");
+    await Sleep(100);
+    test.classList.add("appear");
 }
-
