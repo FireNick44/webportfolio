6a9ba15d73765af10e25b424ac7a4763ce0f8285
FireNick44 | YannicA46 <yannic.studer44@gmail.com>
2022-04-11 08:46:26 +0200

    brick

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var loader = document.querySelector(".loader")
 var content = document.querySelector(".content")
 
-window.addEventListener("load", vanish);
+window.addEventListener("load", sessionStorage);
 
 function Sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
+function sessionStorage(){
+    sessionStorage.setItem('shown', true);
+    if (!sessionStorage.getItem('shown')) vanish();
+}
+
 async function vanish() {
+    console.log("hello");
     await Sleep(2000);
     loader.classList.add("disappear");
     await Sleep(100);
