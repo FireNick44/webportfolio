662e3aab0921976349264a892df49314b3817d5f
FireNick44 | YannicA46 <yannic.studer44@gmail.com>
2022-04-11 18:49:10 +0200

    it worksssssssss (pls)

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 var loader = document.querySelector(".loader")
 var content = document.querySelector(".content")
 
-window.addEventListener("load", sessionStorage);
+window.addEventListener("load", sessionLoadPage());
 
 function Sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-function sessionStorage(){
-    sessionStorage.setItem('shown', true);
-    if (!sessionStorage.getItem('shown')) vanish();
-}
+async function sessionLoadPage() {
+    if(!sessionStorage.getItem('shown')) {
+        await Sleep(2000);
+        loader.classList.add("disappear");
+        await Sleep(100);
+        content.classList.add("appear");
+        sessionStorage.setItem('shown', true);
+        console.log("shown wurde gesetzt");
+    }
+    else if (sessionStorage.getItem('shown')) {
+        content.classList.add("appear");
+        loader.classList.add("disappear");
 
-async function vanish() {
-    console.log("hello");
-    await Sleep(2000);
-    loader.classList.add("disappear");
-    await Sleep(100);
-    content.classList.add("appear");
-}
+    }
+} 
