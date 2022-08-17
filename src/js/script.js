5d23a6a7920fd16fd762685aef0751a6e5dcb338
FireNick44 <yannic.studer@protonmail.com>
2022-08-17 15:19:54 +0200

    time is running + scss file struct + start skill svg + format tab

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,26 @@
-var loader = document.querySelector(".loader")
-var content = document.querySelector(".content")
+var loader = document.querySelector(".loader");
+var content = document.querySelector(".content");
 
 function Sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
 window.addEventListener("onload", sessionOnLoadPage());
 window.addEventListener("load", sessionLoadPage());
 
-async function sessionOnLoadPage() {
-
-}
+async function sessionOnLoadPage() { }
 
 async function sessionLoadPage() {
-    if(!sessionStorage.getItem("pageWasLoaded")) {
-        await Sleep(4000);
-        loader.classList.add("disappear");
-        await Sleep(100);
-        content.classList.add("appear");
-        sessionStorage.setItem("pageWasLoaded", true);
-    }
-    else if (sessionStorage.getItem("pageWasLoaded")) {
-        await Sleep(2000);
-        loader.classList.add("disappear");
-        await Sleep(100);
-        content.classList.add("appear");
-    }
+   if (!sessionStorage.getItem("pageWasLoaded")) {
+      await Sleep(4000);
+      loader.classList.add("disappear");
+      await Sleep(100);
+      content.classList.add("appear");
+      sessionStorage.setItem("pageWasLoaded", true);
+   } else if (sessionStorage.getItem("pageWasLoaded")) {
+      await Sleep(2000);
+      loader.classList.add("disappear");
+      await Sleep(100);
+      content.classList.add("appear");
+   }
 }
