ecf8c58e1945428e8c37c719b8430153417896f5
Y <Yannic_Studer1@sluz.ch>
2023-02-13 21:05:27 +0100

    rework

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,16 @@
+window.addEventListener("onload", sessionLoadPage());
+
 var loader = document.querySelector(".loader");
 var content = document.querySelector(".content");
+var loadedDOM = 0;
+var toggle = document.getElementById("toggle");
 
-function Sleep(milliseconds) {
+async function Sleep(milliseconds) {
    return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
 
-window.addEventListener("onload", sessionOnLoadPage());
-window.addEventListener("load", sessionLoadPage());
-
-async function sessionOnLoadPage() { }
-
 async function sessionLoadPage() {
-   if(sessionStorage.getItem("pageHasLightmode", true)) themeMenu(); //looking for lightmode
+   if(sessionStorage.getItem("pageHasLightmode", true)) loadedDOM = 1;
 
    if (!sessionStorage.getItem("pageWasLoaded")) {
       await Sleep(5000);
@@ -27,73 +26,50 @@ async function sessionLoadPage() {
    }
 }
 
+document.addEventListener('DOMContentLoaded', (event) => {
+   if(loadedDOM == 1) themeKlick();
+})
 
-var body = document.body;
-var cover = document.getElementById("cover");
-var toggle = document.getElementById("toggle");
-var moon = document.getElementById("moon");
-var sun = document.getElementById("sun");
+function themeKlick(){
+   if(!toggle.classList.contains("toggleWrapperOpen")) sessionStorage.setItem("pageHasLightmode", true);  //lightmode
+   else if(toggle.classList.contains("toggleWrapperOpen")) sessionStorage.removeItem("pageHasLightmode"); //Darkmode
 
-var hContact = document.getElementById("head-contact");
-var github = document.getElementById("github");
+   let cover = document.getElementById("cover");
+   let moon = document.getElementById("moon");
+   let sun = document.getElementById("sun");
+   let hContact = document.getElementById("headContact");
+   let github = document.getElementById("github");
+   let me = document.getElementById("me");
+   let wave1 = document.getElementById("wave1");
+   let wave2 = document.getElementById("wave2");
+   let wave3 = document.getElementById("wave3");
+   let brick = document.getElementById("introBrick");
+   let introScroll = document.getElementById("introScroll");
+   let projects = document.getElementById("projects");
+   let fix1 = document.getElementById("fix1");
+   let fix2 = document.getElementById("fix2");
+   let fix3 = document.getElementById("fix3");
+   let byeWave = document.getElementById("byeWave");
 
-var me = document.getElementById("me");
-var wave1 = document.getElementById("wave1");
-var wave2 = document.getElementById("wave2");
-
-var brick = document.getElementById("intro-brick");
-var introScroll = document.getElementById("intro-scroll");
-
-var projects = document.getElementById("projects");
-var wave3 = document.getElementById("wave3");
-
-var fix1 = document.getElementById("fix1");
-var fix2 = document.getElementById("fix2");
-var fix3 = document.getElementById("fix3");
-
-var byeWave = document.getElementById("bye-wave");
-
-function show(){
-   alert("hi")
-}
-
-function themeToggle(){
-   if(cover.classList.contains("cover-open")) sessionStorage.setItem("pageHasLightmode", true); //lightmode
-   else if(!cover.classList.contains("cover-open")) sessionStorage.removeItem("pageHasLightmode") //Darkmode
-   cover.classList.toggle("cover-open");
-   toggle.classList.toggle("toggle-wrapper-open");
+   toggle.classList.toggle("toggleWrapperOpen");
+   cover.classList.toggle("coverOpen");
    moon.classList.toggle("colorLightFill");
-   sun.classList.toggle("colorDarkFill");
-   
+   sun.classList.toggle("colorDarkFill");//
    hContact.classList.toggle("colorDark");
-   github.classList.toggle("colorDarkFill");
-
+   github.classList.toggle("colorDarkFill");//
    me.classList.toggle("colorDarkBG");
    wave1.classList.toggle("colorLightFill");
    wave2.classList.toggle("colorLightFill");
-
-
+   wave3.classList.toggle("colorLightFill");//
    brick.classList.toggle("colorDarkBG");
-   introScroll.classList.toggle("colorDarkFill");
-
-   projects.classList.toggle("colorDarkBG");
-   wave3.classList.toggle("colorLightFill");
-   
-   
+   introScroll.classList.toggle("colorDarkFill");//
+   projects.classList.toggle("colorDarkBG");//
    fix1.classList.toggle("colorDarkBG");
    fix2.classList.toggle("colorDarkBG");
-   fix3.classList.toggle("colorBlack");
-
-   byeWave.classList.toggle("bye-waveLight");
-
+   fix3.classList.toggle("colorBlack");//
+   byeWave.classList.toggle("byeWaveLight");
 }
 
-function themeMenu(){
-   if(cover.classList.contains("cover-open")) sessionStorage.setItem("pageHasLightmode", true); //lightmode
-   else if(!cover.classList.contains("cover-open")) sessionStorage.removeItem("pageHasLightmode") //Darkmode
-   
-   // modemenu.classList.toggle("open");
-   // body.classList.toggle("body-light");
-   // bg.classList.toggle("background-light");
-   // cards1.classList.toggle("card-light");
+function show(){
+   alert("Hi :)")
 }
